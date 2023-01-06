0ae86f13943c431a662e0a489e1c7619c7b6f4c1
usernameKim <akddkwl1310@gmailcom>
2023-01-06 10:58:48 +0900

    20230106_kgm

--- eventi/src/main/java/com/eventi/left/job/web/JobBoardController.java
+++ eventi/src/main/java/com/eventi/left/job/web/JobBoardController.java
@@ -1,5 +1,20 @@
 package com.eventi.left.job.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eventi.left.job.mapper.JobBoardMapper;
+
 public class JobBoardController {
 
+	@Autowired 
+	JobBoardMapper jobMapper;
+	
+	@RequestMapping(value = "/jobList", method=RequestMethod.GET)
+	public String jobList(Model model) {
+		model.addAttribute("jobList", jobMapper.getJobList(null));
+		return "content/job/jobList";
+	}
 }
