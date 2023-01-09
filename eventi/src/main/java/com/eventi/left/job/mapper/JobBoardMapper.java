0ad99aad7950f5601509afb3a4470b47269d9375
usernameKim <akddkwl1310@gmailcom>
2023-01-09 17:31:22 +0900

    20230109_kgm

--- eventi/src/main/java/com/eventi/left/job/mapper/JobBoardMapper.java
+++ eventi/src/main/java/com/eventi/left/job/mapper/JobBoardMapper.java
@@ -6,5 +6,8 @@ import com.eventi.left.job.service.JobBoardVO;
 
 public interface JobBoardMapper {
 
-	public List<JobBoardVO> getJobList(JobBoardVO jobBoardVO); 
+	public List<JobBoardVO> getJobList(JobBoardVO jobBoardVO); //전체조회
+	public JobBoardVO getJob(JobBoardVO jobBoardVO); //단건조회
+	
+
 }
--- eventi/src/main/java/com/eventi/left/resume/service/impl/ResumeServiceImpl.java
+++ eventi/src/main/java/com/eventi/left/resume/service/impl/ResumeServiceImpl.java
@@ -2,15 +2,27 @@ package com.eventi.left.resume.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.eventi.left.resume.mapper.ResumeBoardMapper;
 import com.eventi.left.resume.service.ResumeBoardVO;
 import com.eventi.left.resume.service.ResumeService;
 
 public class ResumeServiceImpl implements ResumeService{
 
+	@Autowired 
+	ResumeBoardMapper resumeMapper;
+	
 	@Override
 	public List<ResumeBoardVO> getResumeList(ResumeBoardVO resumeBoardVO) {
 		// TODO Auto-generated method stub
-		return null;
+		return resumeMapper.getResumeList(resumeBoardVO);
+	}
+
+	@Override
+	public ResumeBoardVO getResume(ResumeBoardVO resumeBoardVO) {
+		// TODO Auto-generated method stub
+		return resumeMapper.getResume(resumeBoardVO);
 	}
 
 }
