b91ac66bd1d595a12079a471957f611203a5dd45
sehun <tpgns5890@naver.com>
2023-01-10 02:33:16 +0900

    자격증 추가 ajax 작성 완료 -> cors 오류 발생(교수님 여쭤봐야함)

--- eventi/src/main/java/com/eventi/left/contest/web/ContestController.java
+++ eventi/src/main/java/com/eventi/left/contest/web/ContestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.eventi.left.common.Paging;
 import com.eventi.left.contest.service.ContestService;
 import com.eventi.left.contest.service.ContestVO;
-import com.eventi.left.contest.service.WinnerVO;
 
 import groovy.util.logging.Log4j;
 
