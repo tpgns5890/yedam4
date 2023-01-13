ec05cd6d072a3db826a1efa26a25d0ae896f472a
hyun <pokemdn@naver.com>
2023-01-13 17:23:33 +0900

    견적요청서 작성페이지 구현중(60%)

--- eventi/src/main/java/com/eventi/left/estimate/mapper/EstMapper.java
+++ eventi/src/main/java/com/eventi/left/estimate/mapper/EstMapper.java
@@ -10,6 +10,7 @@ public interface EstMapper {
 	//견적요청서
 	public EstVO getEst(String eno); // 1건조회
 	public List<EstVO> getEstList(EstVO estVO); // 전체조회
+	public EstVO insertEst(EstVO estVO); //견적요청서 등록
 	
 	//제안서
 	public List<PropVO> getPropList(String eno); // 견적서별 전체조회
