0af50c8098c54b45bb1f8ec87d37321dbe1afc9c
Sehoon Cheon <105044717+tpgns5890@users.noreply.github.com>
2023-01-16 11:09:09 +0900

    Update DesignService.java

--- eventi/src/main/java/com/eventi/left/design/service/DesignService.java
+++ eventi/src/main/java/com/eventi/left/design/service/DesignService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.eventi.left.common.Paging;
 
 public interface DesignService {
-	//디자인 전체 조회
 	public List<DesignVO> designList(DesignVO vo, Paging paging);
 	
 	public List<DesignVO> contestDesignList(String cNo); //공모전 1건에 대한 리스트.
