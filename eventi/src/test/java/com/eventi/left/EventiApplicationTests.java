a0f28de44fa4148f1768e71652c5793493f2ce0d
CH <CH@114.202.178.6>
2023-01-17 07:24:09 +0900

    등록/상세조회 페이지구현중

--- eventi/src/test/java/com/eventi/left/EventiApplicationTests.java
+++ eventi/src/test/java/com/eventi/left/EventiApplicationTests.java
@@ -1,11 +1,23 @@
 package com.eventi.left;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class EventiApplicationTests {
+	
+	 @Test
+	   void contextLoads() {
 
+	    }
 }
