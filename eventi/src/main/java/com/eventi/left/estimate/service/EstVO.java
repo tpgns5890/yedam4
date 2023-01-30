cb95662e929ac623479d4235ea27183b6faf6b03
CH <CH@175.112.21.244>
2023-01-30 13:59:33 +0900

    견적서상세페이지 비회원 접근시 로그인창띄우기/날짜데이터 null출력 해결중

--- eventi/src/main/java/com/eventi/left/estimate/service/EstVO.java
+++ eventi/src/main/java/com/eventi/left/estimate/service/EstVO.java
@@ -1,12 +1,8 @@
 package com.eventi.left.estimate.service;
 
-import java.sql.Date;
-import java.util.List;
+import java.util.Date;
 
-import com.eventi.left.member.service.BusiVO;
-import com.eventi.left.member.service.CrtfVO;
-import com.eventi.left.member.service.MemberVO;
-import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,10 +15,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class EstVO {
 	private String eno; //견적요청서 번호
-	private String userId; // 회원ID
+	private String userId; // 회원ID	
 	private String eventType; // 행사유형
 	private String pats;  // 행사규모
 	private String expectedPl; // 행사예정장소
+	@DateTimeFormat(pattern = "yyyy-MM-dd") 
 	private Date eventDate; // 행사예정일
 	private String eventTime; //행사시작시간
 	private String eventDuration; //행사소요시간
