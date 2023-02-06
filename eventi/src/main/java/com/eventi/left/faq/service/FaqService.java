a9b7cae59715110a99b6837eeb59b444b7235e34
usernameKim <akddkwl1310@gmailcom>
2023-02-06 18:23:12 +0900

    0206

--- eventi/src/main/java/com/eventi/left/faq/service/FaqService.java
+++ eventi/src/main/java/com/eventi/left/faq/service/FaqService.java
@@ -5,5 +5,14 @@ import java.util.List;
 public interface FaqService {
 	
 	//전체조회
-	public List<FaqVO> getFaqList(FaqVO faqVO); 
+	public List<FaqVO> getFaqList(FaqVO faqVO);
+	
+	//등록
+	public int faqInsert(FaqVO faqVO);
+	
+	//시퀀스 값 찾기
+	public String getSeq();
+	
+	//삭제
+	public int faqDelete(FaqVO faqVO);
 }
--- eventi/src/main/java/com/eventi/left/faq/web/FaqController.java
+++ eventi/src/main/java/com/eventi/left/faq/web/FaqController.java
@@ -3,11 +3,14 @@ package com.eventi.left.faq.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.eventi.left.faq.service.FaqService;
 import com.eventi.left.faq.service.FaqVO;
+import com.eventi.left.notice.service.NoticeVO;
 
 @Controller
 public class FaqController {
@@ -22,4 +25,24 @@ public class FaqController {
 		return "content/faq/faqList";
 	}
 	
+	//구인등록폼이동
+	@RequestMapping(value = "/faqInsert", method=RequestMethod.GET) 
+	public String faqInsert(Model model) {
+		model.addAttribute("nextNo", faqService.getSeq());
+		return "content/faq/faqInsert";
+	}
+			
+	//게시글 등록
+	@PostMapping("/faqInsert")
+	public String faqInsert(FaqVO faqVO) {
+		faqService.faqInsert(faqVO);
+		return "redirect:/faqList";
+	}
+	
+	//게시글 삭제
+	@GetMapping("/faqDelete") 
+	public String faqDelete(FaqVO faqVO) {
+		faqService.faqDelete(faqVO);
+		return "redirect:/faqList"; 
+	}	
 }
