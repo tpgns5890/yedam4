54d48b2aaff5ee0d5969f79a7aae706c91555fea
dry3997 <dry3997@naver.com>
2023-02-09 11:04:12 +0900

    공모전 css 페이징 js 파일 분리

---
+++ eventi/src/main/resources/static/assets/js/paging.js
@@ -0,0 +1,45 @@
+// 페이징 만들기
+function makePageing(page) {
+	if (page.totalRecord == 0) {
+		return;
+	}
+	let pagination = $("<div/>")
+	let ul = $("<ul/>").attr("class", "pagination");
+	if (page.startPage > 1) {
+		$(ul).append($("<li/>").html("&laquo;")
+			.css("font-size", "30px")
+			.attr("class", "page-item")
+			.on("click", function() {
+				gopage(page.startPage - page.pageSize)
+			}));
+	}
+
+	//페이지 버튼 생성.
+	for (let i = page.startPage; i <= page.endPage; i++) {
+		if (page.page == i) {
+			$(ul).append($("<li/>").text(i).css("font-size", "20px")
+				.attr("class", "page-link active")
+				.css("border-radius", " 18%")
+				.css("padding", "3px 12px")
+			);
+		} else {
+			$(ul).append($("<li/>").text(i).css("font-size", "20px")
+											.attr("class", "page-link")
+											.css("padding", "3px 12px")
+											.on("click", function() {
+					gopage($(this).text());
+				})); //페이지이동 함수
+		}
+	}
+	if (page.endPage < page.lastPage) {
+		$(ul).append($("<li/>").html("&raquo;")
+			.css("padding", "3px 12px")
+			.css("font-size", "30px")
+			.attr("class", "page-item")
+			.on("click", function() {
+				gopage(page.startPage + page.pageSize)
+			}));
+	}
+	pagination.append(ul);
+	return pagination;
+}
